d50bc3e484d3bd4ac64c4d91351aefc416d56653
Draxx0 <williamfort.lmgl@gmail.com>
2023-02-27 09:29:18 +0100

    [RESOLVE] blob

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
 const noise = new SimplexNoise();
 
-// divise a value
 function fractionate(val, minVal, maxVal) {
     return (val - minVal)/(maxVal - minVal);
 }
 
-// modulate a value
 function modulate(val, minVal, maxVal, outMin, outMax) {
     const fr = fractionate(val, minVal, maxVal);
     const delta = outMax - outMin;
     return outMin + (fr * delta);
 }
 
-// average of an array
 function avg(arr){
     const total = arr.reduce(function(sum, b) { return sum + b; });
     return (total / arr.length);
 }
 
-// max of an array
 function max(arr){
     return arr.reduce(function(a, b){ return Math.max(a, b); })
 }
 
-// Init sphere
 const initSphere = function (){
   
-    // get file, audio and label
     const file = document.querySelector("#thefile");
     const audio = document.querySelector("#audio");
     const fileLabel = document.querySelector("label.file");
     
-    // play audio on load
     document.onload = () => {
         audio.play();
         play();
     }
 
-    // change class on file change
     file.onchange = function(){
         fileLabel.classList.add('normal');
         audio.classList.add('active');
-        var files = this.files;
+        const files = this.files;
         
         audio.src = URL.createObjectURL(files[0]);
         audio.load();
@@ -49,7 +41,6 @@ const initSphere = function (){
         play();
     }
   
-    // play audio
     function play() {
         const context = new AudioContext();
         const src = context.createMediaElementSource(audio);
@@ -107,21 +98,34 @@ const initSphere = function (){
 
         const lowerMaxFr = lowerMax / lowerHalfArray.length;
         const upperAvgFr = upperAvg / upperHalfArray.length;
-        mesh.geometry.verticesNeedUpdate = true;
-        mesh.geometry.normalsNeedUpdate = true;
-        mesh.geometry.computeVertexNormals();
-        mesh.geometry.computeFaceNormals();
+        
+        makeRoughBall(ball, modulate(Math.pow(lowerMaxFr, 0.8), 0, 1, 0, 8), modulate(upperAvgFr, 0, 1, 0, 4));
 
         group.rotation.y += 0.005;
         renderer.render(scene, camera);
         requestAnimationFrame(render);
         }
 
-        // Responsive Blob
         function onWindowResize() {
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
             renderer.setSize(window.innerWidth, window.innerHeight);
+        }
+
+        function makeRoughBall(mesh, bassFr, treFr) {
+            mesh.geometry.vertices.forEach(function (vertex, i) {
+                const offset = mesh.geometry.parameters.radius;
+                const amp = 7;
+                const time = window.performance.now();
+                vertex.normalize();
+                const rf = 0.00001;
+                const distance = (offset + bassFr ) + noise.noise3D(vertex.x + time *rf*7, vertex.y +  time*rf*8, vertex.z + time*rf*9) * amp * treFr;
+                vertex.multiplyScalar(distance);
+            });
+            mesh.geometry.verticesNeedUpdate = true;
+            mesh.geometry.normalsNeedUpdate = true;
+            mesh.geometry.computeVertexNormals();
+            mesh.geometry.computeFaceNormals();
         }
 
         audio.play();
